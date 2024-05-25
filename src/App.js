e367d6b9e43783ec2e33fb16894f77efb2fbd07f
Bryan Pazmino <bryanpazmi1977@gmail.com>
2024-05-25 10:47:20 -0500

    Se agrego el login con la funcionalidad si es admin o user que le lleve a paginas diferentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './login/login.jsx';
 import Inicio from './paginas/inicio.jsx';
-
+//aqui le importas tu pantalla  
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />} ></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/inicio" element={<Inicio />} />
+          //aqui le traes a donde te va a llevar 
+        </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
